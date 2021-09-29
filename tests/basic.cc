56ce95ef27f4a4cd60635f8ea6d555f5ab100d9d
Hedzr Yeh <hedzrz@gmail.com>
2021-09-29 23:11:33 +0800

    added on_error callback for testing

--- tests/basic.cc
+++ tests/basic.cc
@@ -78,6 +78,10 @@ namespace fsm_cxx { namespace test {
         m.add_guard(my_state::Opened, [](event_base const &, M::Context &, M::State const &, M::Payload const &) -> bool { return true; });
         m.add_guard(my_state::Opened, [](event_base const &, M::Context &, M::State const &, M::Payload const &p) -> bool { return p._ok; });
 
+        m.on_error([](Reason reason, M::State const &, M::Context &, M::Event const &, M::Payload const &) {
+            std::cout << "          Error: reason = " << reason << '\n';
+        });
+
         // debug log
         m.on_action_for_debug([&m](auto const &from, auto const &ev, auto const &to, auto const &actions, auto const &payload) {
             std::printf("        [%s] -- %s --> [%s] (payload = %s)\n", m.state_to_sting(from).c_str(), ev.c_str(), m.state_to_sting(to).c_str(), to_string(payload).c_str());
@@ -87,7 +91,7 @@ namespace fsm_cxx { namespace test {
         // processing
 
         m.step_by(begin{});
-        if(!m.step_by(open{}, payload_t{false}))
+        if (!m.step_by(open{}, payload_t{false}))
             std::cout << "          E. cannot step to next with a false payload\n";
         m.step_by(open{});
         m.step_by(close{});
