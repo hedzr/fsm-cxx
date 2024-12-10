6ef6d4c5089d836614cf96fb7ff5ad95e7a9b721
hz <hedzrz@gmail.com>
2024-12-10 08:59:13 +0800

    fixed basic.cc in c++20 and more sticted compiling options

--- tests/basic.cc
+++ tests/basic.cc
@@ -113,7 +113,7 @@ namespace {
     m.step_by(open{});
     m.step_by(end{});
 
-    std::printf("---- END OF test_state_meta()\n\n\n");
+    std::printf("---- END OF test_state_meta() | v=%d\n\n\n", hicc::dp::state::bugs::v);
   }
 
   // TODO 1. hierarchical state
@@ -134,6 +134,8 @@ namespace {
     // using namespace hmeta;
     machine_t<my_state, fsm_cxx::event_t, std::mutex> m;
     using M = decltype(m);
+
+    std::cout << calculator::Empty << '\n';
 
     m.state().set(my_state::Initial).as_initial().build();
     m.state().set(my_state::Terminated).as_terminated().build();
@@ -197,13 +199,16 @@ namespace {
       // （ _1 与 _2 来自 std::placeholders ，并表示将来会传递给 f1 的参数）
       auto f1 = [capture0 = std::cref(n), n](auto &&PH1, auto &&PH2) { f(std::forward<decltype(PH2)>(PH2), 42, std::forward<decltype(PH1)>(PH1), capture0, n); };
       n = 10;
-      f1(1, 2, 1001); // 1 为 _1 所绑定， 2 为 _2 所绑定，不使用 1001
-      // 进行到 f(2, 42, 1, n, 7) 的调用
+      f1(1, 2); // 1 为 _1 所绑定， 2 为 _2 所绑定，不使用 1001
+                // 进行到 f(2, 42, 1, n, 7) 的调用
 
+#if 0 // not for c++20 and higher \
       // 嵌套 bind 子表达式共享占位符
       auto f2 = std::bind(f, _3, [](auto &&, auto &&, auto &&PH3) { return g(std::forward<decltype(PH3)>(PH3)); }, _3, 4, 5);
       f2(10, 11, 12); // 进行到 f(12, g(12), 12, 4, 5); 的调用
+#endif
 
+#if 0 // not for c++20 and higher \
       // 常见使用情况：以分布绑定 RNG
       std::default_random_engine const e;
       std::uniform_int_distribution<> const d(0, 10);
@@ -211,6 +216,7 @@ namespace {
       for (int n1 = 0; n1 < 10; ++n1)
         std::cout << rnd() << ' ';
       std::cout << '\n';
+#endif
 
       // 绑定指向成员函数指针
       Foo foo;
@@ -226,7 +232,6 @@ namespace {
                 << f4(std::make_unique<Foo>(foo)) << '\n';
     }
   }
-
 }
 } // namespace lambdas
 
@@ -252,7 +257,9 @@ int main() {
 
   test_lock_guard();
 
+  UNUSED(lambdas::test_lambdas, lambdas::g);
   // lambdas::test_lambdas();
+
   test_state_basic();
   fsm_cxx::test::test_state_meta();
 
